ea5aee81b7482e71e7de88b30d2a1ca04b594896
Diogo Vinicius <diogo.vinicius@gmail.com>
2021-07-24 17:53:57 -0300

    feat: first register user classes created

---
+++ src/ControleEstoque.Mvc/Controllers/ContaController.cs
@@ -0,0 +1,29 @@
+﻿using ControleEstoque.Mvc.ViewModels;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Mvc.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ src/ControleEstoque.Mvc/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.Mvc.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+        
+        [Required]
+        [Display(Name = "Nome Completo")]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]   
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+    }
+}
