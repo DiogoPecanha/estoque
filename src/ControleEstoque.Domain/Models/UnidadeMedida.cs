0926b0280ede7d7b37369b01b5c81ccb63ab506e
Diogo Vinicius <diogo.vinicius@gmail.com>
2021-07-24 16:37:26 -0300

    setup domain layer

---
+++ src/ControleEstoque.Domain/Models/UnidadeMedida.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.Domain.Models
+{
+    public enum UnidadeMedida
+    {
+        [Display(Name = "Quilograma")]
+        KG = 1,
+        [Display(Name = "Litro")]
+        LT,
+        [Display(Name = "Unidade")]
+        UN,
+        [Display(Name = "Metro linear")]
+        MT,
+        [Display(Name = "Metro quadrado")]
+        M2,
+        [Display(Name = "Metro cúbico")]
+        M3
+    }
+}
