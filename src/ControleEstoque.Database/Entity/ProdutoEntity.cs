ba5eeda0cc8e89e5a7d29be7694b9618ce467f89
Diogo Vinicius <diogo.vinicius@gmail.com>
2021-07-24 18:22:28 -0300

    feat: update nuget packages

---
+++ src/ControleEstoque.Database/Entity/ProdutoEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstoque.Database.Entity
+{
+    public class ProdutoEntity
+    {
+        /// <summary>
+        /// Código do produto
+        /// </summary>
+        public long Codigo { get; set; }
+
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Descrição do produto
+        /// </summary>
+        public string Descricao { get; set; }
+
+        /// <summary>
+        /// Unidade básica de medida do produto
+        /// </summary>
+        public UnidadeMedida UnidadeMedida { get; set; }
+    }
+}
---
+++ src/ControleEstoque.Database/Entity/UnidadeMedidaEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstoque.Database.Entity
+{
+    public enum UnidadeMedida
+    {
+        [Display(Name = "Quilograma")]
+        KG = 1,
+        [Display(Name = "Litro")]
+        LT,
+        [Display(Name = "Unidade")]
+        UN,
+        [Display(Name = "Metro linear")]
+        MT,
+        [Display(Name = "Metro quadrado")]
+        M2,
+        [Display(Name = "Metro cúbico")]
+        M3
+    }
+}
